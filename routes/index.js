a69efe90964d02b94c56f8cb4b25db6f2fc2d219
NicolasP <p1926177@forge.univ-lyon1.fr>
2025-06-03 23:59:09 +0200

    :sparkles: ingredients entity and routes

--- routes/index.js
+++ routes/index.js
@@ -4,4 +4,13 @@ const router = express.Router();
 const { router: authRoutes } = require("./auth");
 router.use("/auth", authRoutes);
 
+const ingredientsRoutes = require("./ingredients");
+router.use("/ingredients", ingredientsRoutes);
+
+const recipesRoutes = require("./recipes");
+router.use("/recipes", recipesRoutes);
+
+const recipeIngredientsRoutes = require("./recipeIngredients");
+router.use("/recipe-ingredients", recipeIngredientsRoutes);
+
 module.exports = router;
